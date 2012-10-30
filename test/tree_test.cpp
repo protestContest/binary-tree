541ed91e7cf7f93e041de38aca0b6342ebaddf56
Zack Michener <zack@zackmichener.com>
2012-10-30 16:19:54 -0400

    fixed gtest syntax errors

--- test/tree_test.cpp
+++ test/tree_test.cpp
@@ -38,59 +38,86 @@ TEST(insert, BasicInsert) {
 
 TEST(find, BasicFind) {
     BinaryTree<int>* it = new BinaryTree<int>();
-    EXPECT_EQ(false, it->find(0));
-    EXPECT_EQ(false, it->find(100));
+    EXPECT_FALSE(it->find(0));
+    EXPECT_FALSE(it->find(100));
     delete it;
 
     BinaryTree<double>* dt = new BinaryTree<double>();
-    EXPECT_EQ(false, dt->find(0.1005001));
-    EXPECT_EQ(false, dt->find(100));
+    EXPECT_FALSE(dt->find(0.1005001));
+    EXPECT_FALSE(dt->find(100));
     delete dt;
 
     BinaryTree<string>* st = new BinaryTree<string>();
-    EXPECT_EQ(false, st->find("hello"));
-    EXPECT_EQ(false, st->find("world"));
+    EXPECT_FALSE(st->find("hello"));
+    EXPECT_FALSE(st->find("world"));
     delete st;
 
 }
 
 TEST(multiple, FindAndInsert) {
     BinaryTree<int>* it = new BinaryTree<int>();
-    EXPECT_EQ(false, it->find(12));
-    EXPECT_EQ(false, it->find(10000));
+    EXPECT_FALSE(it->find(12));
+    EXPECT_FALSE(it->find(10000));
     it->insert(12);
-    EXPECT_EQ(true, it->find(12));
-    EXPECT_EQ(false, it->find(10000));
+    EXPECT_TRUE(it->find(12));
+    EXPECT_FALSE(it->find(10000));
     it->insert(10000);
-    EXPECT_EQ(true, it->find(12));
-    EXPECT_EQ(true, it->find(10000));
+    EXPECT_TRUE(it->find(12));
+    EXPECT_TRUE(it->find(10000));
     it->insert(12);
-    EXPECT_EQ(true, it->find(12));
+    EXPECT_TRUE(it->find(12));
     delete it;
 
     BinaryTree<double>* dt = new BinaryTree<double>();
-    EXPECT_EQ(false, dt->find(1.12));
-    EXPECT_EQ(false, dt->find(10000.01));
+    EXPECT_FALSE(dt->find(1.12));
+    EXPECT_FALSE(dt->find(10000.01));
     dt->insert(1.12);
-    EXPECT_EQ(true, dt->find(1.12));
-    EXPECT_EQ(false, dt->find(10000.01));
+    EXPECT_TRUE(dt->find(1.12));
+    EXPECT_FALSE(dt->find(10000.01));
     dt->insert(10000.01);
-    EXPECT_EQ(true, dt->find(1.12));
-    EXPECT_EQ(true, dt->find(10000.01));
+    EXPECT_TRUE(dt->find(1.12));
+    EXPECT_TRUE(dt->find(10000.01));
     dt->insert(1.12);
-    EXPECT_EQ(true, dt->find(1.12));
+    EXPECT_TRUE(dt->find(1.12));
     delete dt;
 
     BinaryTree<string>* st = new BinaryTree<string>();
-    EXPECT_EQ(false, st->find("asdf"));
-    EXPECT_EQ(false, st->find("hello"));
+    EXPECT_FALSE(st->find("asdf"));
+    EXPECT_FALSE(st->find("hello"));
     st->insert("asdf");
-    EXPECT_EQ(true, st->find("asdf"));
-    EXPECT_EQ(false, st->find("hello"));
+    EXPECT_TRUE(st->find("asdf"));
+    EXPECT_FALSE(st->find("hello"));
     st->insert("hello");
-    EXPECT_EQ(true, st->find("asdf"));
-    EXPECT_EQ(true, st->find("hello"));
+    EXPECT_TRUE(st->find("asdf"));
+    EXPECT_TRUE(st->find("hello"));
     st->insert("asdf");
-    EXPECT_EQ(true, st->find("asdf"));
+    EXPECT_TRUE(st->find("asdf"));
     delete st;
+}
+
+TEST(remove, Remove) {
+    BinaryTree<int>* it = new BinaryTree<int>();
+    EXPECT_FALSE(it->find(12));
+    it->insert(12);
+    EXPECT_TRUE(it->find(12));
+    it->remove(12);
+    EXPECT_FALSE(it->find(12));
+    delete it;
+
+    BinaryTree<double>* dt = new BinaryTree<double>();
+    EXPECT_FALSE(dt->find(-7.12));
+    dt->insert(-7.12);
+    EXPECT_TRUE(it->find(-7.12));
+    dt->remove(-7.12);
+    EXPECT_FALSE(dt->find(-7.12));
+    delete dt;
+
+    BinaryTree<string>* st = new BinaryTree<string>();
+    EXPECT_FALSE(st->find("test"));
+    st->insert("test");
+    EXPECT_TRUE(st->find("test"));
+    st->remove("test");
+    EXPECT_FALSE(st->find("test"));
+    delete st;
+
 }
