b6b2f52a5fe7de232b866d5aa045bff99aebc392
Zack Michener <zack@zackmichener.com>
2012-10-30 09:08:41 -0400

    added some tests

--- test/tree_test.cpp
+++ test/tree_test.cpp
@@ -5,3 +5,27 @@
 using std::cout;
 using std::endl;
 using std::string;
+
+TEST(TreeTest, Constructor) {
+    BinaryTree<int>* it = new BinaryTree();
+    delete it;
+
+    BinaryTree<double>* dt = new BinaryTree();
+    delete dt;
+    
+    BinaryTree<string>* st = new BinaryTree();
+    delete st;
+}
+
+TEST(TreeTest, BasicGetSize) {
+    BinaryTree<int>* it = new BinaryTree();
+    EXPECT_EQ()
+}
+
+TEST(TreeTest, BasicInsert) {
+    BinaryTree<int>* it = new BinaryTree();
+    it->insert(5);
+    it->insert(12);
+    it->insert(15);
+    delete it;
+}
